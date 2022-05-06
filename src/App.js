f3dbe7107ee668c67678e078054c732bc681a3ed
Filipe Farias <filipe.farias.chagas@protonmail.com>
2022-05-06 15:56:11 -0300

    inicialização do projeto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit the file at <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, It's Awesome, Man
         </a>
       </header>
     </div>
