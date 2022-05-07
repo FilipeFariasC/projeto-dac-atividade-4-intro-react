d31e9ae8138db8486fded196726808e6121d50ce
Filipe Farias <filipe.farias.chagas@protonmail.com>
2022-05-07 18:05:06 -0300

    entrega da atividade

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { BraceletModel } from "./components/BraceletModel";
 
 import { useState } from "react";
 
+// Hook
 export default function App() {
-  const [register, setRegister] = useState(false);
+  const [page, setPage] = useState(true);
 
+  // se page for true, mostra o user model
+  // se page for false, mostra o bracelet model
   return (
     <div className="App">
-      {!register ? (
-        <UserModel goToBraceletPage={() => setRegister(true)} />
+      {page ? (
+        <UserModel goToBraceletPage={() => setPage(false)} />
       ) : (
-        <BraceletModel goToUserPage={() => setRegister(false)} />
+        <BraceletModel goToUserPage={() => setPage(true)} />
       )}
     </div>
   );
